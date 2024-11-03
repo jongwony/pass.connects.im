0e07b5dcd9482a0b4fc904f6b17fa118019a79d7
jongwony <lastone9182@gmail.com>
2024-11-03 09:44:58 +0900

    remove unused variable

--- src/app/form/insta_special/page.tsx
+++ src/app/form/insta_special/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { ImageProvider, useImageContext } from '../ImageContext'
+import { ImageProvider } from '../ImageContext'
 import ProfilePictureUpload from "../Profile";
 import ChooseTemplate from '@/app/Template';
 import { DisplayCroppedImage } from '../CropImage';
