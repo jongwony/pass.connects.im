85896be253cb8829f0eef65ee94a855a68b06493
jongwony <lastone9182@gmail.com>
2025-02-23 09:50:47 +0900

    remove Animate, primer to scroll-snap mandatory

--- src/app/form/Template.tsx
+++ src/app/form/Template.tsx
@@ -1,84 +1,45 @@
 "use client"
 
-import React, { useState, useLayoutEffect } from 'react'
-import { motion, AnimatePresence } from "framer-motion"
-import { ChevronLeft, ChevronRight } from "lucide-react"
+import React, { useState, useLayoutEffect, useRef } from 'react'
 import Image from "next/image"
 
 
 interface TemplateProps {
-  templates: {id: string, name: string, src: string, dark: string, light: string}[];
+  templates: { id: string, name: string, src: string, dark: string, light: string }[];
   onThemeChange: (theme: string) => void;
   onTemplateChange: (template: string) => void;
 }
 
 export default function ChooseTemplate({ templates, onThemeChange, onTemplateChange }: TemplateProps) {
   const [currentTemplate, setCurrentTemplate] = useState(0);
-  const [dragStart, setDragStart] = useState(0);
   const [currentTheme, setCurrentTheme] = useState('dark');
-  const [direction, setDirection] = useState(1); // 방향 상태 유지
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
 
   useLayoutEffect(() => {
     onTemplateChange(templates[currentTemplate].id);
     onThemeChange(currentTheme);
   }, [currentTemplate, currentTheme]);
 
+  const handleScroll = () => {
+    if (scrollContainerRef.current) {
+      const scrollLeft = scrollContainerRef.current.scrollLeft;
+      const child = scrollContainerRef.current.children[0] as HTMLElement
+      if (!child) return;
+      const childStyle = getComputedStyle(child)
+      const childWidth = child.offsetWidth + parseInt(childStyle.marginRight)
+      // const templateWidth = scrollContainerRef.current.clientWidth;
+
+      const newCurrentTemplate = Math.round(scrollLeft / childWidth);
+      setCurrentTemplate(newCurrentTemplate);
+      setCurrentTheme(templates[newCurrentTemplate].id)
+      onTemplateChange(templates[newCurrentTemplate].id)
+    }
+  }
+
   const handleThemeChange = (theme: string) => {
     setCurrentTheme(theme)
     onThemeChange(theme)
   }
-
-  const handleNext = () => {
-    setDirection(1); // 다음으로 이동 시 방향을 1로 설정
-    setCurrentTemplate((prev) => {
-      const newIndex = (prev + 1) % templates.length;
-      onTemplateChange(templates[newIndex].id);
-      return newIndex;
-    })
-  }
-
-  const handlePrev = () => {
-    setDirection(-1); // 이전으로 이동 시 방향을 -1로 설정
-    setCurrentTemplate((prev) => {
-      const newIndex = (prev - 1 + templates.length) % templates.length;
-      onTemplateChange(templates[newIndex].id);
-      return newIndex;
-    })
-  }
-
-  const handleDragStart = (event: React.TouchEvent | React.MouseEvent) => {
-    const clientX = "touches" in event ? event.touches[0].clientX : event.clientX
-    setDragStart(clientX)
-  }
-
-  const handleDragEnd = (event: React.TouchEvent | React.MouseEvent) => {
-    const clientX = "changedTouches" in event ? event.changedTouches[0].clientX : event.clientX
-    const diff = dragStart - clientX
-
-    if (Math.abs(diff) > 50) {
-      if (diff > 0) {
-        handleNext()
-      } else {
-        handlePrev()
-      }
-    }
-  }
-
-  // Variants 정의
-  const variants = {
-    enter: (direction: number) => ({
-      x: direction * 300,
-      opacity: 0
-    }),
-    center: {
-      x: 0,
-      opacity: 1
-    },
-    exit: (direction: number) => ({
-      x: -direction * 300,
-      opacity: 0
-    })
-  };
 
   const themeStyles: Record<string, string> = {
     light: "bg-white border-blue-500 scale-110",
@@ -99,75 +60,56 @@ export default function ChooseTemplate({ templates, onThemeChange, onTemplateCha
         <p className="text-sm mb-6 text-zinc-600 dark:text-zinc-400 text-center">{templates[currentTemplate].name}</p>
 
         <div
-          className="relative w-full flex justify-center items-center"
-          onTouchStart={handleDragStart}
-          onTouchEnd={handleDragEnd}
-          onMouseDown={handleDragStart}
-          onMouseUp={handleDragEnd}
+          ref={scrollContainerRef}
+          onScroll={handleScroll}
+          className="relative w-full overflow-x-auto snap-x snap-mandatory flex space-x-4"
         >
-          <AnimatePresence custom={direction} mode="wait">
-            <motion.div
-              key={currentTemplate}
-              custom={direction}
-              variants={variants}
-              initial="enter"
-              animate="center"
-              exit="exit"
-              transition={{ type: "spring", stiffness: 1000, damping: 50 }}
-              className="relative"
-            >
+          {templates.map((template) => (
+            <div key={template.id} className="flex-none snap-center">
               <Image
-                src={templates[currentTemplate].src !== '' ? templates[currentTemplate].src : (currentTheme === 'dark' ? templates[currentTemplate].dark : templates[currentTemplate].light)}
-                alt={templates[currentTemplate].name}
-                className={`w-[282px] h-[568px] object-cover transition-colors duration-100`}
+                src={
+                  template.src !== ''
+                    ? template.src
+                    : currentTheme === 'dark'
+                      ? template.dark
+                      : template.light
+                }
+                alt={template.name}
+                className="w-[282px] h-[568px] object-cover transition-colors duration-100"
                 width={282}
                 height={568}
               />
-            </motion.div>
-          </AnimatePresence>
+            </div>
+          ))}
         </div>
-        <button
-          type="button"
-          onClick={handlePrev}
-          className="absolute left-2 top-1/2 -translate-y-1/2 p-2 rounded-full bg-zinc-200 dark:bg-zinc-800 hover:bg-opacity-80 transition-colors"
-        >
-          <ChevronLeft className="w-4 h-4" />
-        </button>
-        <button
-          type="button"
-          onClick={handleNext}
-          className="absolute right-2 top-1/2 -translate-y-1/2 p-2 rounded-full bg-zinc-200 dark:bg-zinc-800 hover:bg-opacity-80 transition-colors"
-        >
-          <ChevronRight className="w-4 h-4" />
-        </button>
       </div>
- 
+
       <div className="flex justify-center items-center gap-4">
-      {(templates[currentTemplate].src === '') ? (
-        <>
+        {(templates[currentTemplate].src === '') ? (
+          <>
+            <button
+              type="button"
+              onClick={() => handleThemeChange("light")}
+              className={`w-8 h-8 rounded-full border-2 bg-white transition-all ${currentTheme === "light" ? "border-blue-500 scale-110" : "border-gray-200 dark:border-gray-800"
+                }`}
+              aria-label="Light theme"
+            />
+            <button
+              type="button"
+              onClick={() => handleThemeChange("dark")}
+              className={`w-8 h-8 rounded-full border-2 bg-black transition-all ${currentTheme === "dark" ? "border-blue-500 scale-110" : "border-gray-200 dark:border-gray-800"
+                }`}
+              aria-label="Dark theme"
+            />
+          </>
+        ) : (
           <button
             type="button"
-            onClick={() => handleThemeChange("light")}
-            className={`w-8 h-8 rounded-full border-2 bg-white transition-all ${currentTheme === "light" ? "border-blue-500 scale-110" : "border-gray-200 dark:border-gray-800"
-              }`}
-            aria-label="Light theme"
+            className={`w-8 h-8 rounded-full border-2 bg-black transition-all ${getThemeStyle(templates[currentTemplate].id)}`}
+            aria-label="Special theme"
           />
-          <button
-            type="button"
-            onClick={() => handleThemeChange("dark")}
-            className={`w-8 h-8 rounded-full border-2 bg-black transition-all ${currentTheme === "dark" ? "border-blue-500 scale-110" : "border-gray-200 dark:border-gray-800"
-              }`}
-            aria-label="Dark theme"
-          />
-        </>
-      ) : (
-        <button
-          type="button"
-          className={`w-8 h-8 rounded-full border-2 bg-black transition-all ${getThemeStyle(templates[currentTemplate].id)}`}
-          aria-label="Special theme"
-        />
-      )}
+        )}
+      </div>
     </div>
-  </div>
   )
 }
